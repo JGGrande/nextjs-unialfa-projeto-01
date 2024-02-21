26f055225f49ce9672524b5829b5412466800a9e
JGGrande <joaogabriel7303@gmail.com>
2024-02-20 22:25:11 -0300

    Adicionado responsividade de font para dispositivos desktop

--- src/global/theme.global.ts
+++ src/global/theme.global.ts
@@ -6,4 +6,12 @@ export const StyledComponetsGlobalTheme = createGlobalStyle`
     padding: 0;
     font-family: Roboto;
   }
+
+  @media (max-width: 1080px) {
+    font-size: 93.75%;
+  }
+  @media (max-width: 720px) {
+    font-size: 87.25%;
+  }
+
 `
